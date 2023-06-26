4b5074410505593a76e41fafd4060492451292fb
十七 <1791539136@qq.com>
2023-06-26 11:34:32 +0800

    update

--- Bark/bark-server/Procfile
+++
@@ -1,1 +0,0 @@
-web: bin/bark-server -addr 0.0.0.0:$PORT -serverless true
--- Bark/bark-server/deploy/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-ln -sf "/usr/share/zoneinfo/${TZ}" /etc/localtime
-echo "${TZ}" > /etc/timezone
-
-exec "$@"
